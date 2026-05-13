c1be6eab15d2c11af8cc0079dffccf110477808a
mxfh <mxfh@users.noreply.github.com>
2026-05-13 06:45:52 +0000

    deploy: cismet/carma@3c46845e624cd083c111d7df80455b7ab74d4b95

---
+++ 638/stories/assets/LabelMarkers.stories-Cm1n0WY6.js
@@ -0,0 +1,16 @@
+import{j as t}from"./jsx-runtime-DCY474Ph.js";import{L as g,a as u,b as E,R as A,c as R,d as L,P as b}from"./LabelMarkers.story-helpers-BRijA0Gv.js";import"./index-f9CH5uyH.js";import"./draggable-debug-anchor-dzHjQJrf.js";import"./clamp-co6UzHBn.js";import"./pi-BNTPxsWB.js";import"./AnnotationsProvider-DmcZdUJE.js";import"./index-M3SnQqVT.js";import"./index-o4jIgCFn.js";import"./useLineSegmentVisualizers-jXBh6C2B.js";import"./angle-normalization-DerLMp6Q.js";import"./angles-rGK9UtCE.js";import"./geometry2d-CoOpczPD.js";import"./Elevation-T8kaQsec.js";import"./three.module-DONvljao.js";import"./geodetic-BSEHKtgU.js";import"./negative-pi-to-pi-B-FiLZ9M.js";import"./useCesiumPointMoveGizmo-CgZ2hK3e.js";import"./wms-BW9Y-bW-.js";import"./Scene-C4q-h70M.js";import"./private-shims-5e5gEO17.js";import"./useCesiumFovWheelZoom-DuFIZae7.js";import"./maplibre-gl-CWB7pobr.js";import"./CesiumWidget-DlHT24cf.js";import"./ua-parser-ClrqU7mL.js";import"./context-Bgtk6aWc.js";import"./Compact-DY2GpRF9.js";import"./index-okyHx7Tf.js";import"./Picking-Dh0iKbc-.js";import"./carma-guards-UJ2IZNxb.js";import"./ControlButtonStyler-E6yNKOeB.js";import"./svgProjection-__xSi-EC.js";import"./constants-C6-_E4xW.js";/* empty css                                      */import"./config-BQAn8s2i.js";import"./annotation-cursor-overlay-style-DgRX6aHc.js";import"./barmen-background-CMDoHYMC.js";import"./centered-story-frame-an5kV7Cs.js";const sr={title:"Overlay/Labels",args:g,argTypes:u,parameters:{layout:"fullscreen",...E}},e={name:"States and Themes",args:{...A},render:r=>t.jsx(R,{...r})},o={name:"Backgrounds",args:{storyBackground:"urban"},render:r=>t.jsx(L,{...r})},a={name:"Component",render:r=>t.jsx(b,{...r})};var s,m,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: "States and Themes",
+  args: {
+    ...REPRESENTATIVE_CASES_STORY_ARGS
+  },
+  render: args => <LabelStatesAndThemesStory {...args} />
+}`,...(n=(m=e.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var p,i,c;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  name: "Backgrounds",
+  args: {
+    storyBackground: "urban"
+  },
+  render: args => <LabelBackgroundsStory {...args} />
+}`,...(c=(i=o.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var d,S,l;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: "Component",
+  render: args => <PillboxOnlyStory {...args} />
+}`,...(l=(S=a.parameters)==null?void 0:S.docs)==null?void 0:l.source}}};const mr=["LabelStatesAndThemes","LabelBackgrounds","LabelComponent"];export{o as LabelBackgrounds,a as LabelComponent,e as LabelStatesAndThemes,mr as __namedExportsOrder,sr as default};
